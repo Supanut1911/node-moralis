6d545d6a5f0d259ecd6f4a2b3612e94c413d77e9
Supanut1911 <ynotmela@gmail.com>
2023-11-25 11:19:59 +0700

    add getchain util

---
+++ utils/useGetchain.ts
@@ -0,0 +1,18 @@
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+
+const getChain = (chainName: string): EvmChain => {
+  let chain: EvmChain;
+  switch (chainName) {
+    case "sepolia":
+      chain = EvmChain.SEPOLIA;
+      break;
+    case "mumbai":
+      chain = EvmChain.MUMBAI;
+      break;
+    default:
+      chain = EvmChain.SEPOLIA;
+  }
+  return chain;
+};
+
+export default getChain;
