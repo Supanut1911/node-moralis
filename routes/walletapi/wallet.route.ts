40aa839ed4118ccef64c31098b7e3310dffa9135
Supanut1911 <ynotmela@gmail.com>
2023-11-25 11:27:59 +0700

    add get eth price api

--- routes/walletapi/wallet.route.ts
+++ routes/walletapi/wallet.route.ts
@@ -2,13 +2,31 @@
 
 import express, { Request, Response } from "express";
 import WalletService from "./wallet.service";
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
 
+import getChain from "../../utils/useGetchain";
 const router = express.Router();
 const walletService = new WalletService();
 
 router.get("/hello", (req: Request, res: Response) => {
   const response = walletService.hello();
   res.status(200).json({ response });
+});
+
+router.get("/getETHprice", async (req: Request, res: Response) => {
+  let { walletAddress, chain: chainInput } = req.query;
+  walletAddress = walletAddress as string;
+  let targetChain = getChain(chainInput as string);
+  try {
+    const response = await Moralis.EvmApi.token.getTokenPrice({
+      address: walletAddress,
+      chain: targetChain,
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 export default router;
--- utils/useGetchain.ts
+++ utils/useGetchain.ts
@@ -10,7 +10,7 @@ const getChain = (chainName: string): EvmChain => {
       chain = EvmChain.MUMBAI;
       break;
     default:
-      chain = EvmChain.SEPOLIA;
+      chain = EvmChain.ETHEREUM;
   }
   return chain;
 };
