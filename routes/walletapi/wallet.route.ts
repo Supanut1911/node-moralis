10822bb2d2919d097a482282dc306a081cedbf23
Supanut1911 <ynotmela@gmail.com>
2023-11-25 11:32:25 +0700

    refactor getethprice service

--- routes/walletapi/wallet.route.ts
+++ routes/walletapi/wallet.route.ts
@@ -2,31 +2,17 @@
 
 import express, { Request, Response } from "express";
 import WalletService from "./wallet.service";
-import Moralis from "moralis";
-import { EvmChain } from "@moralisweb3/common-evm-utils";
 
 import getChain from "../../utils/useGetchain";
 const router = express.Router();
 const walletService = new WalletService();
 
-router.get("/hello", (req: Request, res: Response) => {
-  const response = walletService.hello();
-  res.status(200).json({ response });
-});
-
 router.get("/getETHprice", async (req: Request, res: Response) => {
   let { walletAddress, chain: chainInput } = req.query;
   walletAddress = walletAddress as string;
-  let targetChain = getChain(chainInput as string);
-  try {
-    const response = await Moralis.EvmApi.token.getTokenPrice({
-      address: walletAddress,
-      chain: targetChain,
-    });
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  chainInput = chainInput as string;
+  const response = await walletService.getETHprice(walletAddress, chainInput);
+  return res.json(response);
 });
 
 export default router;
--- routes/walletapi/wallet.service.ts
+++ routes/walletapi/wallet.service.ts
@@ -1,15 +1,17 @@
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import getChain from "../../utils/useGetchain";
 class WalletService {
-  // public addUser(user: User): User {
-  //   this.users.push(user);
-  //   return user;
-  // }
-
-  // public getUsers(): User[] {
-  //   return this.users;
-  // }
-
-  public hello() {
-    return "hello";
+  public async getETHprice(walletAddress: string, chainInput: string) {
+    const targetChain = getChain(chainInput);
+    try {
+      return await Moralis.EvmApi.token.getTokenPrice({
+        address: walletAddress,
+        chain: targetChain,
+      });
+    } catch (error) {
+      return error;
+    }
   }
 }
 
